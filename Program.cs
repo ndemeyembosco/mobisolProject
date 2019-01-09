78dfa35cb447ff49b94940828316dcdd506a532e
Bosco Ndemeye <ndemeyemm@hendrix.edu>
2019-01-09 12:39:31 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -20,6 +20,6 @@ namespace mobisolProject
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .Build();
+                .Build(); # modify later
     }
 }
